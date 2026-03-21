9751c497d09119c75f0048a3b3f5475e0283a69d
Abru69 <alanhg665@gmail.com>
2026-03-21 07:49:27 -0600

    fix: move availability route before :id param

--- src/modules/appointments/appointments.controller.ts
+++ src/modules/appointments/appointments.controller.ts
@@ -6,38 +6,42 @@ import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { AppointmentsService } from './appointments.service';
 
 @Controller('appointments')
-@UseGuards(JwtAuthGuard)
 export class AppointmentsController {
   constructor(private apptSvc: AppointmentsService) {}
 
+  @Get('availability/:date')
+  getAvailability(@Param('date') date: string) {
+    return this.apptSvc.getAvailabilityForDate(date);
+  }
+
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Request() req: any, @Body() body: any) {
     return this.apptSvc.create(req.user.sub, body);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll(@Request() req: any, @Query('status') status?: string) {
     return this.apptSvc.findAll(req.user.sub, req.user.rol, status);
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Request() req: any, @Param('id') id: string) {
     return this.apptSvc.findOne(id, req.user.sub, req.user.rol);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Request() req: any, @Param('id') id: string, @Body() body: any) {
     return this.apptSvc.update(id, req.user.sub, req.user.rol, body);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.apptSvc.remove(id);
-  }
-
-  @Get('availability/:date')
-  getAvailability(@Param('date') date: string) {
-    return this.apptSvc.getAvailabilityForDate(date);
   }
 }
